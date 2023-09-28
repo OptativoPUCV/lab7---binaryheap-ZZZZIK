2ca65bc05335fd847490cc8f5d1f6d6089042ba5
Replit user <>
2023-09-28 01:08:56 +0000

    new try

--- heap.c
+++ heap.c
@@ -16,9 +16,15 @@ typedef struct Heap{
   int capac;
 } Heap;
 
+/*
+2. Implemente la función `void* heap_top(Heap* pq)`. Esta función retorna el dato con mayor prioridad del montículo. 
+    > Recuerde que este dato se encuentra en la raíz el montículo, es decir, en la primera casilla del arreglo.
+
+*/
 
 void* heap_top(Heap* pq){
-    return NULL;
+  void* raiz = pq->heapArray[0].data;
+  return raiz;
 }
 
 
@@ -31,6 +37,9 @@ void heap_push(Heap* pq, void* data, int priority){
 void heap_pop(Heap* pq){
 
 }
+
+
+
 /*
 1. Implemente la función `Heap* createHeap()`. Esta función crea un nuevo dato de tipo Heap inicializando sus variables. Considere que la capacidad incial es de 3 casillas para el arreglo.
    > Recuerde reservar memoria para el Heap y también para el arreglo `heapArray`.
